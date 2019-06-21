591229e052594a3ce2e9c9bb057f714fbef9b42b
Yuchen Sun <lyudmila@Yuchens-MacBook-Pro.local>
2019-06-21 10:20:08 -0400

    Adjust to fit Lambda

--- crawling.py
+++ crawling.py
@@ -1,143 +1,230 @@
 from lxml.html import document_fromstring
 import time
+import datetime
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+def getKey():
+    with open('myKeys.txt', 'r') as f:
+        line = f.readline()
+        map = line.strip().split(',')
+    return map[0], map[1], map[2]
 
 def crawling(driver, page_name, web_url):
     driver.get(web_url)
-    # time.sleep(1)
+    time.sleep(1)
     html = driver.page_source
-    # with open('crawl_test/'+page_name+'.html', 'w') as f:
+    # with open('../crawl_test/crunchyroll/'+page_name+'.html', 'w') as f:
     #     f.write(html)
     return html
 
-def crawl_9anime(type):
+def crawl_9anime(type, s3):
+    start = datetime.datetime.now()
+
     if (type == 'init') or (type == 'update'):
-        driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-gpu")
+        driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=options)
         ############# Determine the webpage to crawl ############
         url = ''
         a_pages = []
         e_pages = []
+        a_names = []
         if type == 'init':
             url = 'https://www1.9anime.nl/az-list'
+            key = 'init'
         else:
             url = "https://www1.9anime.nl/ongoing"
+            key = datetime.datetime.now().strftime('%Y-%m-%d')
+
+        ############## Build the s3 directories ##############
+        bucketname = 'insightanimedata'
+        s3.put_object(Bucket=bucketname, Key='9anime/'+key+'/')
+        s3.put_object(Bucket=bucketname, Key='9anime/'+key+'/list_pages/')
+        s3.put_object(Bucket=bucketname, Key='9anime/'+key+'/anime_pages/')
+
         driver.get(url)
         ############# Count the total number of pages need to crawl ############
         page_count = driver.find_element_by_xpath('//*[@id="main"]/div/div[3]/div[2]/div[3]/form/span[3]').get_attribute('textContent')
+        if type == 'init':
+            end = int(page_count)+1
+        else:
+            end = 10
         ############# Crawl pages of anime lists ############
-        # for i in range(1, int(page_count)+1):
-        for i in range(1, 5):
+        for i in range(1, end):
+        # 50-150
+        # for i in range(1, 2):
             page = crawling(driver, '9anime_list_page_'+str(i), url+'?page=' + str(i))
             a_pages.append(page)
+            s3.put_object(Bucket=bucketname, Key='9anime/'+key+'/list_pages/'+str(i+1)+'.html', Body=page)
             print('============ 9anime list page count %d ============' % i)
         ############# Crawl page of each anime in the lists ############
             tree = document_fromstring(page)
-            a_names = tree.xpath('//*[@id="main"]/div/div[3]/div[2]/div[2]/div/div/a/text()')
-            a_hrefs = tree.xpath('//*[@id="main"]/div/div[3]/div[2]/div[2]/div/div/a/@href')
+            if type == 'init':
+                temp_names = tree.xpath('//div[@id="main"]/div/div[3]/div[2]/div[2]/div[@class="item"]/div[@class="info"]/a[@class="name"]/text()')
+                temp_hrefs = tree.xpath('//div[@id="main"]/div/div[3]/div[2]/div[2]//div[@class="item"]/div[@class="info"]/a[@class="name"]/@href')
+            else:
+                temp_names = tree.xpath('//div[@id="main"]/div/div[3]/div[2]/div[2]/div[@class="item"]/div[@class="inner"]/a[@class="name"]/text()')
+                temp_hrefs = tree.xpath('//div[@id="main"]/div/div[3]/div[2]/div[2]//div[@class="item"]/div[@class="inner"]/a[@class="name"]/@href')
             # assert len(a_hrefs)== 30,"ERROR: Number of anime on a page is not 30."
-            for j in range(len(a_hrefs)):
+            for j in range(len(temp_hrefs)):
             # for j in range(10):
-                a_names[j] = "_".join(a_names[j].split())
-                e_pages.append(crawling(driver, a_names[j], a_hrefs[j]))
-                print('-------- anime on the page name: %s -------' % a_names[j])
+                temp_names[j] = "_".join(temp_names[j].split())
+                temp_names[j] = temp_names[j].replace('//', '_')
+                temp_names[j] = temp_names[j].replace('/', '_')
+                a_names.append(temp_names[j])
+                ep = crawling(driver, temp_names[j], temp_hrefs[j])
+                e_pages.append(ep)
+                s3.put_object(Bucket=bucketname, Key='9anime/'+key+'/anime_pages/'+temp_names[j]+'.html', Body=ep)
+                print('-------- anime on the page name: %s -------' % temp_names[j])
         driver.close()
         if type == 'init':
             print('Initialize HTMLs of 9anime successfully.')
         else:
             url = "Update HTMLs of 9anime successfully."
+        end = datetime.datetime.now()
+        print (start.strftime('Crawling 9anime starts at %Y-%m-%d %H:%M:%S'))
+        print (end.strftime('Crawling 9anime ends at %Y-%m-%d %H:%M:%S'))
+        print ('Process time = %s' % str(end - start))
+        # print('a_pages size %d, a_names size %d, e_pages size %d' % (len(a_pages), len(a_names), len(e_pages)))
         return a_pages, e_pages, a_names
     else:
         print('Type error for 9anime crawling')
         return False
 
-def crawl_crunchyroll(type):
+def crawl_crunchyroll(type, s3):
+    start = datetime.datetime.now()
+
     if (type == 'init') or (type == 'update'):
-        driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-gpu")
+        driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=options)
         ############# Determine the webpage to crawl ############
         url = ''
         a_pages = []
         e_pages = []
+        a_names = []
         # Initizalize
         if type == 'init':
             url = 'https://www.crunchyroll.com/videos/anime/alpha?group=all'
-            ############# Crawl pages of anime lists ############
-            page = crawling(driver, 'crunchyroll_list', url)
-            a_pages.append(page)
-            print('============ crunchyroll list page ============')
-            ############# Crawl page of each anime in the lists ############
-            tree = document_fromstring(page)
-            a_names = tree.xpath('//*[@id="main_content"]/div[2]/div[1]/ul/li/a/text()')
-            a_hrefs = tree.xpath('//*[@id="main_content"]/div[2]/div[1]/ul/li/a/@href')
-            for j in range(len(a_names)):
-            # for j in range(20):
-                a_names[j] = '_'.join(a_names[j].strip().split())
-                e_pages.append(crawling(driver, a_names[j], 'https://www.crunchyroll.com' + a_hrefs[j]))
-                print('-------- anime on the page name: %s -------' % a_names[j])
-            print('Initialize HTMLs of crunchyroll successfully.')
-        # Update
+            key = 'init'
         else:
             url = 'https://www.crunchyroll.com/videos/anime/updated'
-            ############# Crawl pages of anime lists ############
-            page = crawling(driver, 'crunchyroll_list', url)
-            a_pages.append(page)
-            print('============ crunchyroll ist page ============')
-            ############# Crawl page of each anime in the lists ############
-            tree = document_fromstring(page)
-            a_names = tree.xpath('//*[@id="main_content"]/ul/li/div/a/@title')
-            a_hrefs = tree.xpath('//*[@id="main_content"]/ul/li/div/a/@href')
-            for j in range(len(a_names)):
-            # for j in range(20):
-                a_names[j] = '_'.join(a_names[j].strip().split())
-                e_pages.append(crawling(driver, a_names[j], 'https://www.crunchyroll.com' + a_hrefs[j]))
-                print('-------- anime on the page name: %s -------' % a_names[j])
+            key = datetime.datetime.now().strftime('%Y-%m-%d')
+
+        ############## Build the s3 directories ##############
+        bucketname = 'insightanimedata'
+        s3.put_object(Bucket=bucketname, Key='crunchyroll/'+key+'/')
+        s3.put_object(Bucket=bucketname, Key='crunchyroll/'+key+'/list_pages/')
+        s3.put_object(Bucket=bucketname, Key='crunchyroll/'+key+'/anime_pages/')
+
+        ############# Crawl pages of anime lists ############
+        page = crawling(driver, 'crunchyroll_list', url)
+        a_pages.append(page)
+        s3.put_object(Bucket=bucketname, Key='crunchyroll/'+key+'/list_pages/1.html', Body=page)
+        print('============ crunchyroll list page ============')
+        ############# Crawl page of each anime in the lists ############
+        tree = document_fromstring(page)
+        if type == 'init':
+            temp_names = tree.xpath('//*[@id="main_content"]/div[2]/div[@class="videos-column left"]/ul/li/a/text()')
+            temp_hrefs = tree.xpath('//*[@id="main_content"]/div[2]/div[@class="videos-column left"]/ul/li/a/@href')
+        else:
+            temp_names = tree.xpath('//*[@id="main_content"]/ul/li/div/a/@title')
+            temp_hrefs = tree.xpath('//*[@id="main_content"]/ul/li/div/a/@href')
+        for j in range(len(temp_names)):
+            temp_names[j] = '_'.join(temp_names[j].strip().split())
+            temp_names[j] = temp_names[j].replace('//', '_')
+            temp_names[j] = temp_names[j].replace('/', '_')
+            a_names.append(temp_names[j])
+            ep = crawling(driver, temp_names[j], 'https://www.crunchyroll.com' + temp_hrefs[j])
+            e_pages.append(ep)
+            s3.put_object(Bucket=bucketname, Key='crunchyroll/'+key+'/anime_pages/'+temp_names[j]+'.html', Body=ep)
+            print('-------- anime on the page name: %s -------' % a_names[j])
+        if type == 'init':
+            print('Initialize HTMLs of crunchyroll successfully.')
+        else:
             print('Update HTMLs of crunchyroll successfully.')
         driver.close()
+        end = datetime.datetime.now()
+        print (start.strftime('Crawling crunchyroll starts at %Y-%m-%d %H:%M:%S'))
+        print (end.strftime('Crawling crunchyroll ends at %Y-%m-%d %H:%M:%S'))
+        print ('Process time = %s' % str(end - start))
         return a_pages, e_pages, a_names
     else:
         print('Type error for 9anime crawling')
         return False
 
-def crawl_kissanime(type):
+def crawl_kissanime(type, s3):
+    start = datetime.datetime.now()
+
     if (type == 'init') or (type == 'update'):
-        driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-gpu")
+        driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=options)
         ############# Determine the webpage to crawl ############
         url = ''
         a_pages = []
         e_pages = []
-        page_count = 5
+        a_names = []
         if type == 'init':
             url = 'https://kissanime.ru/AnimeList'
+            key = 'init'
         else:
             url = "https://kissanime.ru/AnimeList/LatestUpdate"
+            key = datetime.datetime.now().strftime('%Y-%m-%d')
+
+        ############## Build the s3 directories ##############
+        bucketname = 'insightanimedata'
+        s3.put_object(Bucket=bucketname, Key='kissanime/'+key+'/')
+        s3.put_object(Bucket=bucketname, Key='kissanime/'+key+'/list_pages/')
+        s3.put_object(Bucket=bucketname, Key='kissanime/'+key+'/anime_pages/')
+
         driver.get(url)
-        time.sleep(10)
+        time.sleep(5)
         driver.get(url)
+        ############# Count the total number of pages need to crawl ############
+        page_count = driver.find_element_by_xpath('//*[@id="leftside"]/div/div[3]/ul/li[5]/a').get_attribute('page')
         if type == 'init':
-            ############# Count the total number of pages need to crawl ############
-            page_count = driver.find_element_by_xpath('//*[@id="leftside"]/div/div[3]/ul/li[5]/a').get_attribute('page')
-        print(page_count)
+            end = int(page_count)+1
+        else:
+            end = 5
+        # print(page_count)
         ############# Crawl pages of anime lists ############
-        # for i in range(1, int(page_count)+1):
-        for i in range(1, 5):
+        for i in range(1, end):
+        # for i in range(80, 130):
             page = crawling(driver, 'kissanime_list_page_'+str(i), url+'?page=' + str(i))
             a_pages.append(page)
+            s3.put_object(Bucket=bucketname, Key='kissanime/'+key+'/list_pages/'+str(i+1)+'.html', Body=page)
             print('============ kissanime list page count %d ============' % i)
         ############# Crawl page of each anime in the lists ############
             tree = document_fromstring(page)
-            a_names = tree.xpath('//*[@id="leftside"]/div/div[2]/div[4]/table/tbody/tr[@class="add"]/td[1]/a/text()')
-            a_hrefs = tree.xpath('//*[@id="leftside"]/div/div[2]/div[4]/table/tbody/tr[@class="add"]/td[1]/a/@href')
+            temp_names = tree.xpath('//div[@id="containerRoot"]/div[@id="container"]/div[@id="leftside"]/div[@class="bigBarContainer"]/div[@class="barContent"]/div/table[@class="listing"]/tbody/tr/td[1]/a/text()')
+            temp_hrefs = tree.xpath('//div[@id="containerRoot"]/div[@id="container"]/div[@id="leftside"]/div[@class="bigBarContainer"]/div[@class="barContent"]/div/table[@class="listing"]/tbody/tr/td[1]/a/@href')
             # assert len(a_hrefs) == 30,"ERROR: Number of anime on a page is not 30."
-            # for i in range(len(a_hrefs)):
-            for j in range(10):
-                a_names[j] = '_'.join(a_names[j].strip().split())
-                e_pages.append(crawling(driver, a_names[j], 'https://kissanime.ru' + a_hrefs[j]))
-                print('-------- anime on the page name: %s -------' % a_names[j])
+            for j in range(len(temp_hrefs)):
+            # for j in range(10):
+                temp_names[j] = "_".join(temp_names[j].split())
+                temp_names[j] = temp_names[j].replace('//', '_')
+                temp_names[j] = temp_names[j].replace('/', '_')
+                a_names.append(temp_names[j])
+                ep = crawling(driver, temp_names[j], 'https://kissanime.ru' + temp_hrefs[j])
+                e_pages.append(ep)
+                s3.put_object(Bucket=bucketname, Key='kissanime/'+key+'/anime_pages/'+temp_names[j]+'.html', Body=ep)
+                print('-------- anime on the page name: %s -------' % temp_names[j])
         driver.close()
         if type == 'init':
             print('Initialize HTMLs of Kissanime successfully.')
         else:
             url = "Update HTMLs of Kissanime successfully."
+        end = datetime.datetime.now()
+        print (start.strftime('Crawling Kissanime starts at %Y-%m-%d %H:%M:%S'))
+        print (end.strftime('Crawling Kissanime ends at %Y-%m-%d %H:%M:%S'))
+        print ('Process time = %s' % str(end - start))
         return a_pages, e_pages, a_names
     else:
         print('Type error for Kissanime crawling')
@@ -145,39 +232,35 @@ def crawl_kissanime(type):
 
 def crawl_web(web_name, type):
     assert (type == 'init') or (type == 'update'), 'Type ERROR for Crawling: Please input "init" or "update"'
+    ACCESS_KEY, SECRET_KEY, ACCOUNT_REGION = getKey()
+    s3 = boto3.client('s3',aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+    print('Connect to S3 successfully')
     if web_name == '9anime':
-        return crawl_9anime(type)
+        crawl_9anime(type, s3)
     elif web_name == 'crunchyroll':
-        return crawl_crunchyroll(type)
+        crawl_crunchyroll(type, s3)
     elif web_name == 'kissanime':
-        return crawl_kissanime(type)
+        crawl_kissanime(type, s3)
     # add more website
     else:
         print('No such website')
         return -1
 
-def extract():
-    driver = webdriver.Chrome()
-    url = 'https://kissanime.ru/AnimeList?page=1'
-############# Crawl pages of anime lists ############
-    page = crawling(driver, 'kissanime_list_page_1', url)
-    print('============ kissanime list page ============')
-############# Crawl page of each anime in the lists ############
-    tree = document_fromstring(page)
-    print(tree)
-    a_names = tree.xpath('//*[@id="leftside"]/div/div[2]/div[4]/table[@class="listing"]/tbody/tr[2]/td[1]/a/text()')
-    a_hrefs = tree.xpath('//*[@id="leftside"]/div/div[2]/div[4]/table[@class="listing"]/tbody/tr[2]/td[1]/a/@href')
-    # assert len(a_hrefs) == 30,"ERROR: Number of anime on a page is not 30."
-    # for i in range(len(a_hrefs)):
-    # for j in range(len(a_names)):
-    #     a_names[j] = '_'.join(a_names[j].strip().split())
-    #     a_hrefs[j] = 'https://kissanime.ru' + a_hrefs[j]
-    print(a_names)
-    print(a_hrefs)
-    # print(len(a_names))
-    # print(len(a_hrefs))
-    # driver.get(a_hrefs[3])
-    driver.close()
+
+# crawl_web('kissanime', 'update')
+# def extract():
+#     driver = webdriver.Chrome()
+#     url = 'https://www.crunchyroll.com/videos/anime/alpha?group=all'
+#     ############# Crawl pages of anime lists ############
+#     page = crawling(driver, 'crunchyroll_list', url)
+#     # a_pages.append(page)
+#     # print('============ crunchyroll list page ============')
+#     ############# Crawl page of each anime in the lists ############
+#     tree = document_fromstring(page)
+#     a_names = tree.xpath('//*[@id="main_content"]/div[2]/div[@class="videos-column left"]/ul/li/a/text()')
+#     print(a_names)
+#     driver.close()
 
 # extract()
 # crawl_web('kissanime', 'init')
+# crawl_crunchyroll('init')

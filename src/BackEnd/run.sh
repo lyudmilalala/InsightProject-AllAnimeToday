448fafea79c95769127c1a9fd18eb8a5ec16aa80
Yuchen Sun <lyudmila@Yuchens-MacBook-Pro.local>
2019-07-11 17:44:37 -0400

    add info pictures

--- src/BackEnd/run.sh
+++ src/BackEnd/run.sh
@@ -12,19 +12,24 @@ curl https://intoli.com/install-google-chrome.sh
 
 # Place functions on Lambda
 mkdir LambdaBuilder
+cd LambdaBuilder
 
-mkdir LambdaBuilder/lambdaL
+mkdir lambdaL
 cp -r psycopg2/ lambdaL
-pip3 install --target ./LambdaBuilder/lambdaL lxml
-zip -r9 loadtoDB.zip lambdaL/
+pip3 install --target ./lambdaL lxml
+cd lambdaL
+zip -r9 ../loadtoDB.zip .
+cd ../
 zip -g loadtoDB.zip loadtoDB.py
-aws lambda create-function  --function-name loadtoDB --zip-file fileb://loadtoDB.zip --role <your-lambda-user-arn> --handler loadtoDB.lambda_handler --runtime python3.6 --timeout 900 --vpc-config SubnetIds= <comma-separated-your-vpc-subnet-ids>,SecurityGroupIds= <your-security-group-ids>
+aws lambda create-function  --function-name loadtoDB --zip-file fileb://loadtoDB.zip --role <your-lambda-user-arn> --handler loadtoDB.lambda_handler --runtime python3.6 --timeout 900
 
-mkdir LambdaBuilder/lambdaU
+mkdir lambdaU
 cp -r psycopg2/ lambdaU
-zip -r9 loadtoDB.zip lambdaL/
-zip -g loadtoDB.zip loadtoDB.py
-aws lambda create-function  --function-name userUpdate --zip-file fileb://userUpdate.zip --role <your-lambda-user-arn> --handler userUpdate.lambda_handler --runtime python3.6 --timeout 600 --vpc-config SubnetIds= <comma-separated-your-vpc-subnet-ids>,SecurityGroupIds= <your-security-group-ids>
+cd lambdaU
+zip -r9 ../userUpdate.zip .
+cd ../
+zip -g userUpdate.zip userUpdate.py
+aws lambda create-function  --function-name userUpdate --zip-file fileb://userUpdate.zip --role <your-lambda-user-arn> --handler userUpdate.lambda_handler --runtime python3.6 --timeout 600
 
 # Create SNS topic that lambda functions listen to
 aws sns create-topic --name ExtractTrigger
@@ -32,7 +37,8 @@ aws sns create-topic --name UpdateTrigger
 aws sns create-topic --name MessageSenderTrigger
 
 # Start the FrontEnd website
-export FLASK_APP= WebContent/frontEnd.py
+cd ../../
+export FLASK_APP= ./WebContent/frontEnd.py
 sudo python3 WebContent/frontEnd.py
 # to keep the FrontEnd website running at backstage, replace the above command with
 # nohup sudo python3 WebContent/frontEnd.py & [1] 24823
